a9376429aefe65893a7ff3c86a03f9a09ccf0add
Smart6502 <69001578+Smart6502@users.noreply.github.com>
2021-05-13 16:35:07 +0530

    Circles: example and screenshot

--- tests/ellipse.c
+++ tests/ellipse.c
@@ -6,6 +6,7 @@ int main()
     pluto_canvas_t *canvas = pluto_init_canvas(0);
     pluto_clear(canvas);
     pluto_draw_ellipse(canvas, (pt_t){60, 60}, 60 / 2, 60 / 3);
-    pluto_draw_circle(canvas, (pt_t){60, 60}, 45);
+    pluto_draw_circle(canvas, (pt_t){60, 60}, 50);
+    pluto_draw_circle(canvas, (pt_t){95, 70}, 20);
     pluto_free_canvas(canvas);
 }
