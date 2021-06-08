f3282e91c01be14f31e2170604f0ee3f72da1f2b
Smart6502 <69001578+Smart6502@users.noreply.github.com>
2021-06-08 08:42:38 +0530

    density changes

--- examples/density.c
+++ examples/density.c
@@ -10,14 +10,17 @@ const struct timespec wts = {
 	.tv_nsec = (wait % 100000) * 1000
 };
 
+const float density = 0.6;
+const int count = 4;
+
 int main()
 {
     pluto_init_window(true);
 
     float x, y;
-    for (x = 1; x < _pluto_canvas.cwidth / 4; x += 0.5)
+    for (x = 1; x < _pluto_canvas.cwidth / count; x += density)
     {
-	for (y = 1; y < _pluto_canvas.cheight / 2; y += 0.5)
+	for (y = 1; y < _pluto_canvas.cheight / 2; y += density)
 	{
             int r = (int)(x / y);
 	    pluto_set_pix(r * x, r * y);
