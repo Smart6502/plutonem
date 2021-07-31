f8255b5378f36051c1a79eb457cbb90b7dbaa64d
Smart6502 <69001578+Smart6502@users.noreply.github.com>
2021-07-31 12:21:59 +0530

    Sierpinski

---
+++ examples/sierpinski.c
@@ -0,0 +1,56 @@
+#include <math.h>
+#include <pluto.h>
+#include <stdlib.h>
+
+typedef struct
+{
+	float x, y;
+} point;
+
+const int sx = 10;	// Start X
+const int ex = 60;	// End X
+const int ty = 10;	// Top Y
+const int by = 90;	// Extended bottom Y
+const int my = 90;	// Extended max Y
+const int iter = 1000;	// Iterations
+const int mid = sx + (ex - sx) / 2;
+
+point vx[3] = { {sx, ty}, {ex, ty}, {mid, 0} };
+point init = {mid, 0};
+
+void step()
+{
+	int vt = rand() % 3;
+
+	init = (point){ init.x + (vx[vt].x - init.x) / 2,
+			init.y + (vx[vt].y - init.y) / 2 };
+
+	pluto_set_pix(init.x, init.y);
+}
+
+void animate()
+{
+	for (int c = by; c < my; c++)
+	{
+		vx[2].y = c;
+		init.y = (c - vx[0].y) / 2;
+
+		for (int i = 0; i < iter; i++)
+			step();
+
+		pluto_write_out();
+		pluto_render();
+		pluto_clear_buffers();
+	}
+}
+
+int main()
+{
+	pluto_init_window();
+	
+	animate();
+
+	pluto_deinit();
+
+	return 0;
+}
